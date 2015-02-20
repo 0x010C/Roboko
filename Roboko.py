11aae6b46d72342084ac2b8aee00b2c9221ef883
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2015-02-20 03:30:40 +0100

    Nouvelle section du manga café + N'affiche les modifs que des ips

--- Roboko.py
+++ Roboko.py
@@ -24,7 +24,7 @@ port = 6667;
 
 wait = 30;
 cat = "https://fr.wikipedia.org/w/api.php?hidebots=1&days=7&limit=50&target=Catégorie:Portail:Animation+et+bande+dessinée+asiatiques/Articles+liés&hidewikidata=1&action=feedrecentchanges&feedformat=atom";
-page = "";
+page = "https://fr.wikipedia.org/w/index.php?title=Discussion_Projet:Animation_et_bande_dessin%C3%A9e_asiatiques&feed=atom&action=history";
 
 old_timestamp1 = calendar.timegm(time.gmtime());
 old_timestamp2 = calendar.timegm(time.gmtime());
@@ -62,7 +62,7 @@ class mybot(ircbot.SingleServerIRCBot):
 	
 	def checker(self):
 		self.check_new_article(cat);
-		#check_new_section(self, bistro);
+		self.check_new_section(page);
 		self.saveServ.execute_delayed(wait, self.checker);
 	
 	def jisho(self, message):
@@ -71,7 +71,7 @@ class mybot(ircbot.SingleServerIRCBot):
 	# Les checkers
 	def check_new_article(self, cat_link):
 		global old_timestamp1;
-		print "\n-->check with " + str(int(old_timestamp1)) + "\n";
+		print "\n-->check at " + str(int(old_timestamp1)) + "\n";
 		timestamp1 = calendar.timegm(time.gmtime());
 		entries = get_new_entries(cat_link, old_timestamp1);
 		for item in entries:
@@ -81,11 +81,28 @@ class mybot(ircbot.SingleServerIRCBot):
 				self.act(chan, tmp.encode('utf-8'));
 				time.sleep(2);
 			else:
-				tmp = u"- Modification de l'article [["+ item.title + u"]] - " + article_link(item.title);
+				if isIp(item.author):
+					tmp = u"- Modification de [["+ item.title + u"]] par " + item.author + u" - https:" + item.id;
+					print tmp;
+					self.act(chan, tmp.encode('utf-8'));
+					time.sleep(2);
+		old_timestamp1 = timestamp1;
+		
+	def check_new_section(self, page_link):
+		global old_timestamp2;
+		print "\n==#check at " + str(int(old_timestamp2)) + "\n";
+		timestamp2 = calendar.timegm(time.gmtime());
+		entries = get_new_entries(page_link, old_timestamp2);
+		conn = httplib.HTTPSConnection("fr.wikipedia.org");
+		for item in entries:
+			conn.request("GET", item.id[24:]);
+			print item.id[24:];
+			if re.search('<td class="diff-addedline"><div>==.+==</div></td>', conn.getresponse().read()):
+				tmp = u"- Nouveau sujet sur le Manga Café :-) https://fr.wikipedia.org/wiki/Discussion_Projet:Animation_et_bande_dessinée_asiatiques#footer";
 				print tmp;
 				self.act(chan, tmp.encode('utf-8'));
 				time.sleep(2);
-		old_timestamp1 = timestamp1;
+		old_timestamp2 = timestamp2;
 
 
 # Gestion du temps
@@ -96,19 +113,28 @@ def timestampisation(date):
 # Recuperation de feed rss
 def get_entries(link):
 	feed = parse(link);
-	return feed;
+	return feed['entries'];
 
 def get_new_entries(link, old_timestamp):
 	feed = get_entries(link);
 	entries = [];
-	for item in feed['entries']:
+	for item in feed:
 		if int(timestampisation(item.updated)-3600) > int(old_timestamp):
 			entries.append(item);
 	return entries;
 
 # Divers
 def article_link(link):
-	return "https://fr.wikipedia.org/wiki/" + link;
+	return "https://fr.wikipedia.org/wiki/" + link.replace(" ", "_");
+
+def isIp(name):
+	if re.search("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$", name):
+		return True;
+	else:
+		if re.search("^[0-9a-f]{0,4}:[0-9a-f]{0,4}:[0-9a-f]{0,4}:[0-9a-f]{0,4}:[0-9a-f]{0,4}:[0-9a-f]{0,4}:[0-9a-f]{0,4}:[0-9a-f]{0,4}$", name):
+			return True;
+		else:
+			return False;
 
 # Main
 def main():
