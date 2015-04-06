c9164cf052010850ebf3039d97c15bad4eddfd6e
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2015-04-06 23:46:29 +0200

    Information des nouvelles news sur Animeland et AnimeNewsNetwork

--- Roboko.py
+++ Roboko.py
@@ -17,7 +17,7 @@ import unicodedata;
 import urllib;
 
 #Paramètres
-version = "1.27"
+version = "1.28"
 chan = "";
 pseudo = "";
 password = "";
@@ -31,6 +31,7 @@ page = "https://fr.wikipedia.org/w/index.php?title=Discussion_Projet:Animation_e
 
 old_timestamp1 = calendar.timegm(time.gmtime());
 old_timestamp2 = calendar.timegm(time.gmtime());
+old_timestamp3 = calendar.timegm(time.gmtime());
 
 
 # Boucle principale + Gestion de la lecture et de l'écriture IRC
@@ -121,6 +122,7 @@ class mybot(ircbot.SingleServerIRCBot):
 		print "### Checker";
 		self.check_new_article(cat);
 		self.check_new_section(page);
+		self.check_new_news();
 		self.saveServ.execute_delayed(wait, self.checker);
 
 
@@ -128,7 +130,7 @@ class mybot(ircbot.SingleServerIRCBot):
 	def check_new_article(self, cat_link):
 		global old_timestamp1;
 		timestamp1 = calendar.timegm(time.gmtime());
-		entries = get_new_entries(cat_link, old_timestamp1);
+		entries = get_new_entries_atom(cat_link, old_timestamp1);
 		for item in entries:
 			if re.search("\n<p><b>Nouvelle page</b></p>", item.summary):
 				tmp = u"\00313\002Nouvel article\002\003 : [[\00307"+ item.title + u"\003]] – \00310" + article_link(item.title.encode('utf-8')) + "\003";
@@ -145,7 +147,7 @@ class mybot(ircbot.SingleServerIRCBot):
 	def check_new_section(self, page_link):
 		global old_timestamp2;
 		timestamp2 = calendar.timegm(time.gmtime());
-		entries = get_new_entries(page_link, old_timestamp2);
+		entries = get_new_entries_atom(page_link, old_timestamp2);
 		conn = httplib.HTTPSConnection("fr.wikipedia.org");
 		for item in entries:
 			conn.request("GET", item.id[24:]);
@@ -159,6 +161,22 @@ class mybot(ircbot.SingleServerIRCBot):
 					self.send(chan, tmp.encode('utf-8'));
 					time.sleep(2);
 		old_timestamp2 = timestamp2;
+		
+	def check_new_news(self):
+		global old_timestamp3;
+		timestamp3 = calendar.timegm(time.gmtime());
+
+		entries = get_new_entries_rss(u"http://www.animenewsnetwork.com/news/rss.xml", old_timestamp3);
+		for item in entries:
+			tmp = u"[ANN] "+item.title+u" – http://4nn.cx/"+item.link.split("/")[-1];
+			self.send(chan, tmp.encode('utf-8'));
+
+		entries = get_new_entries_rss(u"http://www.animeland.com/rss/news", old_timestamp3);
+		for item in entries:
+			tmp = u"[Animeland] "+item.title+u" – "+u"/".join(item.link.replace(u" ", u"_").split(u"/")[:-1])+u"/";
+			self.send(chan, tmp.encode('utf-8'));
+
+		old_timestamp3 = timestamp3;
 
 	def jisho(self, message):
 		conn = httplib.HTTPConnection("tangorin.com");
@@ -185,20 +203,42 @@ class mybot(ircbot.SingleServerIRCBot):
 # Gestion du temps
 def timestampisation(date):
 	return time.mktime(time.strptime(date,"%Y-%m-%dT%H:%M:%SZ"));
+	
+def timestampisation2(date):
+	tz = int(re.findall("^.+([-+][0-9]{2})[0-9]{2}$", date)[0]);
+	d = re.findall("^[a-zA-Z]+, (.+) [-+][0-9]{4}$", date)[0];
+	return time.mktime(time.strptime(d,"%d %b %Y %H:%M:%S")) - (tz*3600);
 
-
-# Recuperation de feed rss
+# Recuperation de feed rss/atom brut
 def get_entries(link):
 	feed = parse(link);
 	return feed['entries'];
 
-def get_new_entries(link, old_timestamp):
+# Recuperation de feed rss
+def get_new_entries_rss(link, old_timestamp):
+	try:
+		feed = get_entries(link);
+	except:
+		print "Unable to get "+link;
+		feed = []
+	entries = [];
+	for item in feed:
+		try:
+			if int(timestampisation2(item.published)) > int(old_timestamp): # Attention, le serveur doit être en UTC
+				entries.append(item);
+		except:
+			print "bad date";
+	return entries;
+
+# Recuperation de feed atom
+def get_new_entries_atom(link, old_timestamp):
 	feed = get_entries(link);
 	entries = [];
 	for item in feed:
-		if int(timestampisation(item.updated)) > int(old_timestamp):
+		if int(timestampisation(item.updated)) > int(old_timestamp): # Attention, le serveur doit être en UTC
 			entries.append(item);
 	return entries;
+
 
 # Divers
 def article_link(link):
@@ -283,3 +323,4 @@ def main():
 		time.sleep(30);
 
 main();
+print timestampisation2("Fri, 03 Apr 2015 08:20:20 -0400");
