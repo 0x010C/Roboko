efe6114a8d679cd1dc873a6c16cae018f178d77f
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2015-07-11 00:25:03 +0200

    Issue #15 : small bugfix

--- Roboko_seen.py
+++ Roboko_seen.py
@@ -27,10 +27,9 @@ def get(irc, user):
 	global seentable;
 	user = user.split(" ")[0];
 	print "#"+user+"#";
+	if user in irc.channels[irc.chan].users():
+		return u"Regardes mieux, " + user + " est sur le chan ;)"
 	if user in seentable:
-		if user in irc.channels[irc.chan].users():
-			return u"Regardes mieux, " + user + " est sur le chan ;)"
-		else:
-			return user + u" a été aperçu pour la dernière fois il y a " + rbk_utils.timestampToInterval(seentable[user]["timestamp"]) + ", le " + rbk_utils.timestampToDate(seentable[user]["timestamp"]) + " (" + seentable[user]["message"] + ")";
+		return user + u" a été aperçu pour la dernière fois il y a " + rbk_utils.timestampToInterval(seentable[user]["timestamp"]) + ", le " + rbk_utils.timestampToDate(seentable[user]["timestamp"]) + " (" + seentable[user]["message"] + ")";
 	else:
 		return u"Je n'ai jamais vu cet utiliateur...";
